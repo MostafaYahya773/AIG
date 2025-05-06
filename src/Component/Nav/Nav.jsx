3d7551ba3f10253c6e8144919c166143d1c8ff02
MostafaYahya773 <mostafayahya822@gmail.com@users.noreply.github.com>
2025-05-06 11:14:03 +0300

    finish

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -10,7 +10,7 @@ import { CallContext } from '../../Context/Context';
 export default function Nav() {
   let { language, setLanguage } = useContext(CallContext);
   let [isClick, useIsClick] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div>
       <nav className="bg-[#063772] fixed left-0 w-full top-0 right-0 z-50">
@@ -68,11 +68,9 @@ export default function Nav() {
               </a>
             </div>
             <button
-              data-collapse-toggle="navbar-cta"
+              onClick={() => setMenuOpen(!menuOpen)}
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-cta"
-              aria-expanded="false"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -94,7 +92,9 @@ export default function Nav() {
           </div>
 
           <div
-            className="md:flex items-center justify-between hidden w-full md:w-auto  "
+            className={`md:flex items-center justify-between w-full md:w-auto ${
+              menuOpen ? 'block' : 'hidden'
+            }`}
             id="navbar-cta"
           >
             <ul className="flex flex-col lg:text-[20px] md:text-[18px] gap-x-10 justify-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg text-white md:space-x-8 rtl:space-x-reverse md:flex-row  md:mt-0 md:border-0 ">
